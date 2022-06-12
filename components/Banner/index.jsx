f0620f08a5a68898e784c1c295543bbd69c11897
Vasile Novatchii <vasile.novatchii@proovia.co.uk>
2022-06-12 20:08:04 +0300

    fix: remove chakra container

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -1,7 +1,6 @@
 import {
 	Box,
 	Button,
-	Container,
 	Heading,
 	Text,
 	useColorModeValue
@@ -17,58 +16,57 @@ export const Banner = (props) => {
 	};
 
 	return (
-		<Container padding={0} maxW="100%">
+		<Box
+			as="section"
+			h="40vh"
+			w="100%"
+			bg={`url(${imgUrl}) center/cover no-repeat`}
+			filter=""
+			display="flex"
+			alignItems="center"
+			padding={5}
+			_after={{
+				content: '""',
+				pos: 'absolute',
+				left: 0,
+				w: '100%',
+				h: 'inherit',
+				bgGradient:
+					'linear-gradient(to right, hsl(0deg 0% 0% / 24%), hsl(171deg 4% 30% / 70%))'
+			}}
+		>
 			<Box
-				h="40vh"
-				w="100%"
-				bg={`url(${imgUrl}) center/cover no-repeat`}
-				filter=""
+				w={['100%', 500, '50vw']}
 				display="flex"
-				alignItems="center"
-				padding={5}
-				_after={{
-					content: '""',
-					pos: 'absolute',
-					left: 0,
-					w: '100%',
-					h: 'inherit',
-					bgGradient:
-						'linear-gradient(to right, hsl(0deg 0% 0% / 24%), hsl(171deg 4% 30% / 70%))'
-				}}
+				flexDir="column"
+				alignItems="flex-start"
+				color="gray.50"
+				zIndex={1}
 			>
-				<Box
-					w={['100%', 500, '50vw']}
-					display="flex"
-					flexDir="column"
-					alignItems="flex-start"
-					color="gray.50"
-					zIndex={1}
-				>
-					<Heading as="h1" noOfLines={1} mb={2} size="2xl">
-						{title}
-					</Heading>
-					<Text noOfLines={2} mb={3} fontSize="lg">
-						{subTitle}
-					</Text>
-					<Box>
-						<Button
-							pr="6"
-							colorScheme={btnColorScheme}
-							leftIcon={
-								<Image
-									src="/static/play-icon.svg"
-									alt="play icon"
-									width={32}
-									height={32}
-								/>
-							}
-							onClick={handleOnClick}
-						>
-							Play
-						</Button>
-					</Box>
+				<Heading as="h1" noOfLines={1} mb={2} size="2xl">
+					{title}
+				</Heading>
+				<Text noOfLines={2} mb={3} fontSize="lg">
+					{subTitle}
+				</Text>
+				<Box>
+					<Button
+						pr="6"
+						colorScheme={btnColorScheme}
+						leftIcon={
+							<Image
+								src="/static/play-icon.svg"
+								alt="play icon"
+								width={32}
+								height={32}
+							/>
+						}
+						onClick={handleOnClick}
+					>
+						Play
+					</Button>
 				</Box>
 			</Box>
-		</Container>
+		</Box>
 	);
 };
