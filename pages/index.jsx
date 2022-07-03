92542da51ca14ad5593d81983311ede1d3939f7b
Vasile Novatchii <vasile.novatchii@proovia.co.uk>
2022-07-03 13:23:22 +0300

    feat: open modal window when hit the play button over banner

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,6 +22,7 @@ export default function Home({ popular, disney, travel }) {
 				title="Banner title"
 				subTitle="here will be the banner subtitle"
 				imgUrl="https://images.pexels.com/photos/1070945/pexels-photo-1070945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+				videoId="venrE8gdz30"
 			/>
 			<CardSection title="Popular" data={popular} size="large" />
 			<CardSection title="Disney" data={disney} />
