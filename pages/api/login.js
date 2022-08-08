d53c80a935f4c071e64501035d73a8aabd5b0ad2
Vasile Novatchii <vasile.novatchii@proovia.co.uk>
2022-08-08 22:39:03 +0300

    create jwt token

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import { magicAdmin } from '../../lib/magic-server';
 
 export default async function login(req, res) {
@@ -5,11 +6,27 @@ export default async function login(req, res) {
 		try {
 			const auth = req.headers.authorization;
 			const didToken = auth ? auth.substr(7) : '';
-			console.log({ didToken });
 
 			// invoke magic
 			const metadata = await magicAdmin.users.getMetadataByToken(didToken);
 			console.log({ metadata });
+
+			// create jwt
+			const token = jwt.sign(
+				{
+					...metadata,
+					iat: Math.floor(Date.now() / 1000),
+					exp: Math.floor((Date.now() / 1000) * 7 * 24 * 60 * 60),
+					'https://hasura.io/jwt/claims': {
+						'x-hasura-allowed-roles': ['user', 'admin'],
+						'x-hasura-default-role': 'user',
+						'x-hasura-user-id': `${metadata.issuer}`
+					}
+				},
+				process.env.JWT_SECRET_KEY
+			);
+
+			console.log({ token });
 
 			res.send({ done: true });
 		} catch (error) {
